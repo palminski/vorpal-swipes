bebe683d83317560e6b0869673db74214090a4ce
palminski <williambolls98@gmail.com>
2023-07-14 15:45:03 -0700

    added a way to set starting position and velocity when changing levels

--- Assets/Scripts/ControllerScripts/Portal.cs
+++ Assets/Scripts/ControllerScripts/Portal.cs
@@ -7,11 +7,15 @@ public class Portal : MonoBehaviour
 {
     [SerializeField]
     private string target;
+
+    [SerializeField]
+    private string enterPosition = "center";
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("touching");
         if (other.gameObject.CompareTag("Player"))
         {
+            StaticVars.setEntryPosition(enterPosition);
             SceneManager.LoadScene(target);
         }
     }
