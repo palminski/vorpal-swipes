d6e8f3a8a6086ad07514c049483fd9391691804a
palminski <williambolls98@gmail.com>
2023-07-29 15:09:56 -0700

    fixed bug allowing player to be the same color for both A and B

--- Assets/Scripts/InteractableScripts/ColorChanger.cs
+++ Assets/Scripts/InteractableScripts/ColorChanger.cs
@@ -17,6 +17,9 @@ public class ColorChanger : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
         playerColorChange = player.GetComponent<PlayerColorChange>();
+
+        //This converts the color to an HTML RGB string and back so that it matches the process dont to the player pref colors and prevents very slight color variatoins making player essentially able to be same color twice'
+        spriteRenderer.color = (ConvertColorHTML(spriteRenderer.color));
     }
 
     // Update is called once per frame
@@ -24,11 +27,21 @@ public class ColorChanger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            
-            if (playerColorChange.colorA != spriteRenderer.color && playerColorChange.colorB != spriteRenderer.color) {
+            Debug.Log(playerColorChange.colorA + " " + playerColorChange.colorB);
+            Debug.Log("this color: " + spriteRenderer.color);
+            if (playerColorChange.colorA != spriteRenderer.color && playerColorChange.colorB != spriteRenderer.color)
+            {
+                Debug.Log("color changed");
                 playerColorChange.SetNewColor(spriteRenderer.color);
             }
 
         }
+    }
+
+    private Color ConvertColorHTML(Color colorToConvert) {
+
+        ColorUtility.TryParseHtmlString("#"+ColorUtility.ToHtmlStringRGB(colorToConvert), out colorToConvert);
+        
+        return colorToConvert;
     }
 }
