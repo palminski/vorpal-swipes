98b454d37793886722b39ceb7212af55e953c51c
palminski <williambolls98@gmail.com>
2023-08-04 00:58:00 -0700

    updated title page

--- Assets/TextScroll.cs
+++ Assets/TextScroll.cs
@@ -9,22 +9,14 @@ public class TextScroll : MonoBehaviour
     private PlayerInput playerInput;
     private InputAction tapAction;
 
-    [SerializeField]
-    private float scrollSpeed = 0.05f;
-
-    [SerializeField]
-    private int startingMoveDelay = 100;
-
-    private int moveDelay;
     
 
-    private Vector3 startingPosition;
+
     
     //======================================================
     void Awake()
     {
-        startingPosition = transform.position;
-        moveDelay = startingMoveDelay;
+
         playerInput = GetComponent<PlayerInput>();
         tapAction = playerInput.actions.FindAction("Tap");
     }
@@ -40,25 +32,14 @@ public class TextScroll : MonoBehaviour
         tapAction.performed -= Tap;
     }
 
-    private void resetPosition() {
-        moveDelay = startingMoveDelay;
-        transform.position = startingPosition;
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (moveDelay > 0) moveDelay--;
-        if (moveDelay <= 0) transform.position += new Vector3(0, scrollSpeed, 0);
-    }
+
 
     private void Tap(InputAction.CallbackContext context) {
 
-        if (moveDelay > 0 ) {
+
             ControllerScript.Controller.LoadNextLevel("MainRoom");
-        }
-        else {
-            resetPosition();
-        }
+
+
     }
 }
