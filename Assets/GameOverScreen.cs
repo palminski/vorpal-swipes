1190e854eeeae180edf96c289503fd669037ca57
palminski <williambolls98@gmail.com>
2023-08-04 12:28:57 -0700

    text scrolls on game over screen

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 
 
 public class GameOverScreen : MonoBehaviour
@@ -9,11 +9,42 @@ public class GameOverScreen : MonoBehaviour
     private PlayerInput playerInput;
     private InputAction tapAction;
 
+    private TextMeshProUGUI gameOverText;
+    [SerializeField]
+    private TextMeshProUGUI gameOverMessage;
+
+
+    private string gameOverString;
+    private int i = 0;
+
+
+    private Color colorForText;
+
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        gameOverText = GetComponent<TextMeshProUGUI>();
+        gameOverString = gameOverText.text;
         tapAction = playerInput.actions.FindAction("Tap");
+        ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("colorA", "07FF00"), out colorForText);
+    }
+
+    void Start()
+    {
+        gameOverText.color = colorForText;
+        gameOverMessage.color = colorForText;
+
+        gameOverText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        if (i < gameOverString.Length)
+        {
+            gameOverText.text += gameOverString[i];
+            i++;
+        }
     }
 
     private void OnEnable()
@@ -23,14 +54,14 @@ public class GameOverScreen : MonoBehaviour
 
     private void OnDisable()
     {
-        
         tapAction.performed -= Tap;
     }
 
-        private void Tap(InputAction.CallbackContext context) {
+    private void Tap(InputAction.CallbackContext context)
+    {
 
 
-            ControllerScript.Controller.LoadNextLevel("MainRoom");
+        ControllerScript.Controller.LoadNextLevel("MainRoom");
 
 
     }
