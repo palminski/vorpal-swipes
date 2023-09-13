b5bb33d2176ce8027bf25acd8fca077920fdefeb
palminski <williambolls98@gmail.com>
2023-09-13 15:25:13 -0700

    player no longer can turn into dash state without moving

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -14,20 +14,29 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private AudioClip leapSound;
 
+    private Collider2D collisionBox;
+
 
 
     private Vector2 still = new Vector2(0, 0);
+    private int wallLayerMask;
+
+    void Start()
+    {
+        collisionBox = GetComponent<Collider2D>();
+        wallLayerMask = LayerMask.GetMask("Walls");
+    }
 
     private void FixedUpdate()
     {
-        
+
         if (Rigidbody.velocity == still)
         {
             animator.ResetTrigger("Up");
             animator.ResetTrigger("Down");
             animator.ResetTrigger("Right");
             animator.ResetTrigger("Left");
-            
+
             if (lastDirection == new Vector2(0, 1))
             {
                 animator.SetTrigger("Up");
@@ -61,40 +70,43 @@ public class PlayerMovement : MonoBehaviour
 
         if (Rigidbody.velocity.normalized * -1 != direction)
         {
+            
+            
+
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, direction, collisionBox.bounds.extents.y + 0.1f, wallLayerMask);
+            if (!raycast || animator.GetBool("Moving"))
+            {
+                animator.SetBool("Moving", true);
+                //Rotate Player
+                //down
+                if (direction == new Vector2(0, -1))
+                {
+                    gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 0);
+
+                }
+                //right
+                else if (direction == new Vector2(1, 0))
+                {
+                    gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 90);
+
+                }
+                //up
+                else if (direction == new Vector2(0, 1))
+                {
+                    gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 180);
+
+
+                }
+                //left
+                else if (direction == new Vector2(-1, 0))
+                {
+                    gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 270);
+
+                }
+            }
             Rigidbody.velocity = new Vector2(direction.x * jumpForce, direction.y * jumpForce);
             lastDirection = direction;
             if (leapSound) ControllerScript.Controller.PlaySound(leapSound);
-            animator.SetBool("Moving", true);
-            //Rotate Player
-            //down
-            if (direction == new Vector2(0, -1))
-            {
-                gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 0);
-                
-
-            }
-            //right
-            else if (direction == new Vector2(1, 0))
-            {
-                gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 90);
-
-            }
-            //up
-            else if (direction == new Vector2(0, 1))
-            {
-                gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 180);
-                
-
-            }
-            //left
-            else if (direction == new Vector2(-1, 0))
-            {
-                gameObject.transform.rotation = Quaternion.Euler(Vector3.forward * 270);
-
-            }
-
-
-
         }
 
 
